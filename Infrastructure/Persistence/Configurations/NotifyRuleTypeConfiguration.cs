ecf47ae89e92228e30e1b69515a7af2bc5bae7a0
khashayar pakkhesal <k.pakkhesal@alibaba.ir>
2025-01-30 20:18:10 +0330

    Implement config of NotifyRule

---
+++ Infrastructure/Persistence/Configurations/NotifyRuleTypeConfiguration.cs
@@ -0,0 +1,23 @@
+using Domain.NotifyRule.Models;
+using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class NotifyRuleTypeConfiguration : IEntityTypeConfiguration<NotifyRule>
+{
+    public void Configure(EntityTypeBuilder<NotifyRule> builder)
+    {
+        builder.ToTable(nameof(NotifyRule));
+        builder.HasKey(a => a.Id);
+
+        builder.Property(x => x.Name);
+
+        builder.Property(x => x.ViableQuantity)
+            .HasColumnName("ViableQuantity")
+            .IsRequired()
+            .HasConversion(prop => prop.Value,
+                value => Quantity.Create(value));
+    }
+}
