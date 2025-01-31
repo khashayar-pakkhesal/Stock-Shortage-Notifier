32aa07b01539d6b1055e037aba2e56d221c94db3
khashayar pakkhesal <k.pakkhesal@alibaba.ir>
2025-01-31 16:43:02 +0330

    implement ProductRepository.cs

--- Infrastructure/Persistence/Repositories/ProductRepository.cs
+++ Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -6,8 +6,15 @@ namespace Infrastructure.Persistence.Repositories;
 
 public class ProductRepository(StockShortageDbContext dbContext) : IProductRepository
 {
-    public async Task<List<Product>> GetAllAsync()
+    public async Task<List<Product>> GetAllAsync(CancellationToken cancellationToken)
     {
-        return await dbContext.Products.ToListAsync();
+        return await dbContext.Products.ToListAsync(cancellationToken);
+    }
+
+    public async Task<Product> GetByIdAsync(int productId, CancellationToken cancellationToken)
+    {
+        return await dbContext.Products
+            .Where(x => x.Id == productId)
+            .FirstAsync(cancellationToken);
     }
 }
